16974dd37eabe6d4e8b7dc47d08bdc015fc8f93e
piexlmax <303176530@qq.com>
2022-04-08 14:37:26 +0800

    change pb key

--- dongtai-grpc-go/client/main.go
+++ dongtai-grpc-go/client/main.go
@@ -5,18 +5,19 @@ import (
 	"fmt"
 	_ "github.com/HXSecurity/DongTai-agent-go/run/base"
 	_ "github.com/HXSecurity/DongTai-agent-go/run/grpc"
-	DontaiGRPC "github.com/HXSecurity/dongtai-grpc-range/dongtai-grpc-go/pb/go-pb"
+	DongtaiGRPC "github.com/HXSecurity/dongtai-grpc-range/dongtai-grpc-go/pb/go-pb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:8888", grpc.WithInsecure())
+	conn, err := grpc.Dial("localhost:8888", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	client := DontaiGRPC.NewGRPCServiceClient(conn)
-	req, _ := client.Send(context.Background(), &DontaiGRPC.Request{
+	client := DongtaiGRPC.NewGRPCServiceClient(conn)
+	req, _ := client.Send(context.Background(), &DongtaiGRPC.Request{
 		Text: "发送测试数据",
 	})
 	fmt.Println(req)
--- dongtai-grpc-go/server/main.go
+++ dongtai-grpc-go/server/main.go
@@ -4,21 +4,21 @@ import (
 	"context"
 	_ "github.com/HXSecurity/DongTai-agent-go/run/base"
 	_ "github.com/HXSecurity/DongTai-agent-go/run/grpc"
-	DontaiGRPC "github.com/HXSecurity/dongtai-grpc-range/dongtai-grpc-go/pb/go-pb"
+	DongtaiGRPC "github.com/HXSecurity/dongtai-grpc-range/dongtai-grpc-go/pb/go-pb"
 	"google.golang.org/grpc"
 	"net"
 	"os/exec"
 )
 
 type GRPCServer struct {
-	DontaiGRPC.UnimplementedGRPCServiceServer
+	DongtaiGRPC.UnimplementedGRPCServiceServer
 }
 
-func (c *GRPCServer) Send(context context.Context, request *DontaiGRPC.Request) (*DontaiGRPC.Response, error) {
+func (c *GRPCServer) Send(context context.Context, request *DongtaiGRPC.Request) (*DongtaiGRPC.Response, error) {
 	comd := request.Text
 	cmd := exec.Command(comd)
 	cmd.Run()
-	return &DontaiGRPC.Response{
+	return &DongtaiGRPC.Response{
 		State:   200,
 		Message: "someThing",
 	}, nil
@@ -27,7 +27,7 @@ func (c *GRPCServer) Send(context context.Context, request *DontaiGRPC.Request) 
 func main() {
 	l, _ := net.Listen("tcp", ":8888")
 	s := grpc.NewServer()
-	DontaiGRPC.RegisterGRPCServiceServer(s, &GRPCServer{})
+	DongtaiGRPC.RegisterGRPCServiceServer(s, &GRPCServer{})
 	err := s.Serve(l)
 	if err != nil {
 		return
